495ceba25f43d6147c8d18901180adc306733018
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2019-08-07 18:15:19 +0530

    Connect

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-    $mysqli = new mysqli("localhost","root","","miniproject");
+    $mysqli = new mysqli("localhost","root","password","miniproject");
 
     if($mysqli->connect_error){
         die("ERROR: ".$mysqli->connect_error);
